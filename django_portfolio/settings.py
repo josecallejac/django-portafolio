27bb9b09fe6b6480be36943b286d2d5de3f5b7bf
josecalleja <jose@tslatam.com>
2024-11-13 23:36:49 -0300

    allowed hosts

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -127,3 +127,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+ALLOWED_HOSTS = ['localhost', 'web-production-fbc1.up.railway.app']
